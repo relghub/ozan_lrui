71d0c812379f0bfa0aef30f93aa276c0ca3238cf
ozanba <ozanbagirann@gmail.com>
2024-11-10 20:43:20 +0100

    first

---
+++ app/src/main/java/com/example/loginregisterui/RegisterPageActivity.kt
@@ -0,0 +1,31 @@
+package com.example.loginregisterui
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class RegisterPageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.register_page)
+
+        val loginLabel = findViewById<TextView>(R.id.loginLabel)
+        loginLabel.setOnClickListener {
+            Log.d("onboarding", "Login  pressed")
+
+
+            val intent = Intent(this, MainActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP
+            startActivity(intent)
+
+
+        }
+
+    }
+}
