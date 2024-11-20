bfe992e0a5fef46c3692143bd03c5bb8160a5440
Serkan Bayram <serkanbay02@hotmail.com>
2024-11-20 17:11:34 +0100

    Validate input values and show erros accordingly

--- app/src/main/java/com/example/loginregisterui/MainActivity.kt
+++ app/src/main/java/com/example/loginregisterui/MainActivity.kt
@@ -3,11 +3,13 @@ package com.example.loginregisterui
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
@@ -24,10 +26,35 @@ class MainActivity : AppCompatActivity() {
 
 
         val inputEmail = findViewById<TextInputLayout>(R.id.InputEmail);
-        inputEmail.error = "Something went wrong";
-
         val inputPasword = findViewById<TextInputLayout>(R.id.InputPassword);
-        inputPasword.error = "Password is wrong";
+
+        val inputEmailText = findViewById<TextInputEditText>(R.id.InputEmailText).text;
+        val inputPasswordText = findViewById<TextInputEditText>(R.id.InputPasswordText).text;
+
+        val nextButton = findViewById<Button>(R.id.next_button);
+        nextButton.setOnClickListener {
+
+            val isEmailValid = CredentialsManager().isEmailValid(inputEmailText.toString());
+            val isPasswordValid = CredentialsManager().isPasswordValid(inputPasswordText.toString());
+
+            if(!isEmailValid) {
+                inputEmail.isErrorEnabled = true;
+                inputEmail.error = "Email is invalid!";
+            }else{
+                inputEmail.isErrorEnabled = false;
+            }
+
+            if(!isPasswordValid) {
+                inputEmail.isErrorEnabled = true;
+                inputPasword.error = "Password is invalid!";
+            }else{
+                inputEmail.isErrorEnabled = false;
+            }
+
+        }
+
+
+
 
 
 
